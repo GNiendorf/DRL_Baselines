aad6094d8c6aac927f99e88a50c15749a2e4e806
Gavin Niendorf <gavinniendorf@Gavins-MacBook-Pro.local>
2020-01-06 11:45:09 -0800

    updated DQN with Double DQN

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -1,28 +1,45 @@
-import traceback
-import copy
-
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.optimizers import *
+from tensorflow.keras.initializers import VarianceScaling
+from tensorflow.keras.optimizers import RMSprop
 
 import gym
 from baselines.common.atari_wrappers import wrap_deepmind
 
-memory_cap = 100000
-output_freq = 10
-batch_size = 64
-frames_init = 1e4
-total_episodes = 1e3
-gamma = 0.99
-e_max = 1.
-e_min = 0.01
-exp_frames = 5e3
-update_freq = 1e3
+#Tunable Hyperparameters
+memory_cap = 50000
+exp_frames = 50000
+update_freq = 1000
+frames_init = 50000
+batch_size = 32
+e_min = 0.1
+lr = .00025
 
-env = gym.make("CartPole-v0")
-model_path = "./cartpole.h5"
+#Model Choices
+ddqn = True
+target_network = True
+model_path = "./atari.h5"
 load_model = False
+
+if ddqn and not target_network:
+    raise Exception()
+
+#Env Choice
+atari = True
+env_name = 'BreakoutDeterministic-v4'
+
+#Assumed Hyperparameters
+output_freq = 100
+total_episodes = 1e6
+gamma = 0.99
+e_max = 1.0
+
+if atari:
+    env = wrap_deepmind(gym.make(env_name), frame_stack=True, clip_rewards=False)
+else:
+    env = gym.make(env_name)
 
 class memory:
     def __init__(self, N):
@@ -37,120 +54,142 @@ class memory:
             self.memory[self.next_idx] = sars_tuple
         self.next_idx = (self.next_idx + 1) % self.N
         
-    def sample_memory(self, n):
-        indices = np.random.choice(len(self.memory), size=np.min([n, len(self.memory)]))
-        return np.array([self.memory[idx] for idx in indices])
-
-def gen_samples(model, target_model, sars_tuples, gamma):
-    default = np.zeros(env.observation_space.shape)
-    targets = np.zeros((len(sars_tuples), env.action_space.n))
-
-    terminal = [1 if s is None else 0 for s in sars_tuples[:,-1]]
-    S_b = np.array([s for s in sars_tuples[:,0]])
-    Sb = np.array([s if s is not None else default for s in sars_tuples[:,-1]])
-
-    S_targets = model.predict(S_b)
-    Stargets = target_model.predict(Sb)
-
-    for idx in range(len(sars_tuples)):
-        s_ = S_b[idx]
-        a = sars_tuples[idx,1]
-        r = sars_tuples[idx,2]
-        s = Sb[idx]
-        term = terminal[idx]
-        
-        tar = S_targets[idx]
-        if term == 0:
-            target = Stargets[idx]
-            max_target = np.max(target)
-            tar[a] = r + gamma*max_target
-        else:
-            tar[a] = r
-        
-        targets[idx] = tar
+    def sample_memory(self, batch_size):
+        indices = np.random.choice(len(self.memory), size=np.min([batch_size, len(self.memory)]))
+        sampled_memory = self.decode_sample(indices)
+        return sampled_memory
     
-    return S_b, targets
+    def decode_sample(self, indices):
+        s_, a, r, s, term = [], [], [], [], []
+        for i in indices:
+            data = self.memory[i]
+            obs_t, action, reward, obs_tp1, done = data
+            s_.append(np.array(obs_t, copy=False))
+            a.append(np.array(action, copy=False))
+            r.append(reward)
+            s.append(np.array(obs_tp1, copy=False))
+            term.append(done)
+        return (np.array(s_), np.array(a), np.array(r), np.array(s), np.array(term))
+
+def gen_targets(model, target_model, sars_tuples):
+    targets = np.zeros((len(sars_tuples[1]), env.action_space.n))
+
+    state_preds = model.predict(sars_tuples[0])
+    double_state_preds = model.predict(sars_tuples[3])
+    next_state_preds = target_model.predict(sars_tuples[3])
+
+    for idx in range(len(sars_tuples[1])):
+        s_ = sars_tuples[0][idx]
+        a = sars_tuples[1][idx]
+        r = sars_tuples[2][idx]
+        s = sars_tuples[3][idx]
+        term = sars_tuples[4][idx]
+
+        state_pred = state_preds[idx]
+        double_state_pred = double_state_preds[idx]
+        next_state_pred = next_state_preds[idx]
+        
+        double_a = np.argmax(double_state_pred)
+
+        if term == 0:
+            if ddqn:
+                state_pred[a] = r + gamma*next_state_pred[double_a]
+            elif target_network:
+                state_pred[a] = r + gamma*np.max(next_state_pred)
+            else:
+                state_pred[a] = r + gamma*np.max(double_state_pred)
+        else:
+            state_pred[a] = r
+        
+        targets[idx] = state_pred
+    
+    return targets
 
 def create_model():
-    model = keras.Sequential([
-        keras.layers.Dense(64, activation='relu'),
-        keras.layers.Dense(env.action_space.n, activation='linear')
-    ])
+    if atari:
+        model = keras.Sequential([
+            keras.layers.Conv2D(16, (8,8), strides=4, activation='relu', kernel_initializer=VarianceScaling(), input_shape=(84, 84, 4)),
+            keras.layers.Conv2D(32, (4,4), strides=2, activation='relu', kernel_initializer=VarianceScaling()),
+            keras.layers.Flatten(),
+            keras.layers.Dense(256, activation='relu', kernel_initializer=VarianceScaling()),
+            keras.layers.Dense(env.action_space.n, activation='linear', kernel_initializer=VarianceScaling())
+        ])
+    else:
+        model = keras.Sequential([
+            keras.layers.Dense(64, activation='relu'),
+            keras.layers.Dense(env.action_space.n, activation='linear')
+        ])
     return model
 
 if load_model:
     model = keras.models.load_model(model_path)
-    target_model = keras.models.load_model(model_path)
+    target_model = keras.models.clone_model(model)
+    target_model.set_weights(model.get_weights())
 else:
-    target_model = create_model()
     model = create_model()
+    target_model = keras.models.clone_model(model)
+    target_model.set_weights(model.get_weights())
 
-target_model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['acc'])
-model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['acc'])
+target_model.compile(optimizer=RMSprop(lr=lr, rho=0.95, epsilon=0.01), loss='huber_loss', metrics=['acc'])
+model.compile(optimizer=RMSprop(lr=lr, rho=0.95, epsilon=0.01), loss='huber_loss', metrics=['acc'])
 
-max_avg = 0.
+max_avg = -1e4
 time_steps= 0.
 rewards = []
+Q = []
 bank = memory(N = memory_cap)
-Q_vals = []
 
 for episode in range(int(total_episodes)):
-    s = env.reset()
+    s_ = env.reset()
     R = 0
+    done = False
 
-    for t in range(10000):
-        if load_model == False:
+    while done is False:
+        if load_model is False:
             epsilon = np.max([(e_min-e_max)/exp_frames*(time_steps - frames_init) + 1, e_min])
         else:
-            epsilon = 0.01
+            epsilon = 0.05
             env.render()
 
-        if np.random.randn() < epsilon or (len(bank.memory) < frames_init and load_model == False):
+        if np.random.rand() < epsilon:
             a = env.action_space.sample()
         else:
-            q_vals = model.predict(np.expand_dims(s, axis=0))
-            Q_vals.append(np.mean(q_vals))
+            q_vals = model.predict(np.array(s_)[None])
+            Q.append(np.mean(q_vals))
             a = np.argmax(q_vals)
 
-        s_ = copy.deepcopy(s)
-        s, r, done, info = env.step(a)      
-        
-        if done:
-            s = None
-            
+        s, r, done, info = env.step(a)
+
         R += r
         time_steps += 1
 
-        sars = [s_, a, r, s]
+        sars = [s_, a, r, s, done]
         bank.add_memory(sars)
-        
-        if len(bank.memory) > frames_init and load_model == False:
-            sars_tuples = bank.sample_memory(n=batch_size)
-            states, targets = gen_samples(model, target_model, sars_tuples, gamma)
-            model.fit(states, targets, verbose=0)
+        s_ = s
 
-        if time_steps % update_freq == 0 and time_steps > frames_init and load_model == False:
+        if len(bank.memory) >= frames_init and load_model is False:
+            sars_tuples = bank.sample_memory(batch_size=batch_size)
+            targets = gen_targets(model, target_model, sars_tuples)
+            model.fit(sars_tuples[0], targets, verbose=0, batch_size=batch_size, epochs=1)
+
+        if time_steps % update_freq == 0. and len(bank.memory) >= frames_init and load_model is False:
             print("updating target model!")
             target_model.set_weights(model.get_weights())
-        
-        if done:
-            break
 
     rewards.append(R)
-    if episode % output_freq == 0 and episode > 0 and load_model == False:
-        np.savetxt('qvals.txt', Q_vals)
+    if episode % output_freq == 0 and episode > 0:
         np.savetxt('rewards.txt', rewards)
         print("\n")
         print("------------------------------------------")
-        print("Current Epsilon: %.2f" % epsilon)
-        print("Episodes: ", episode)
-        print("Steps: %.2E" % time_steps)
+        print("Current Epsilon: {}".format(epsilon))
+        print("Episodes: {}".format(episode))
+        print("Steps: {}".format(time_steps))
         avg = np.sum(rewards[-output_freq:])/output_freq
-        print("Average reward for last %.0f episodes : %.2f" % (output_freq, avg))
+        print("Average reward for last {} episodes : {}".format(output_freq, avg))
         print("------------------------------------------")
         print("\n")
-        if avg > max_avg:
-            print("Average Reward increased from %s to %s, saving model." % (max_avg, avg))
+        if avg > max_avg and load_model is False:
+            print("Average Reward increased from {} to {}, saving model.".format(max_avg, avg))
             model.save(model_path)
             max_avg = avg
 
